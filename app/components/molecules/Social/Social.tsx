bedc846991c1a678493ac5e62d9643a63ee30b9c
saadbjit <saadman.sayeed@bjitgroup.com>
2023-07-04 15:55:30 +0600

    Added hero and skills

---
+++ app/components/molecules/Social/Social.tsx
@@ -0,0 +1,31 @@
+import Link from 'next/link'
+import React from 'react'
+import { FiGithub,FiFacebook,FiLinkedin,FiTwitter } from "react-icons/fi";
+type Props = {}
+
+export default function Social({}: Props) {
+  return (
+    <>
+    <div className="social">
+         
+    <Link className="social-icons" href="https://github.com/smansayeed" target="_blank">
+      <FiGithub fill="gray"/>
+    </Link>
+
+    <Link className="social-icons" href="https://www.linkedin.com/in/saadmansayeed/" target="_blank">
+      <FiLinkedin fill="skyblue"/>
+    </Link>
+
+    <Link className="social-icons" href="https://twitter.com/Sman_sayeed" target="_blank">
+      <FiTwitter fill="orange" />
+    </Link>
+
+    <Link className="social-icons" href="https://facebook.com/saadman2" target="_blank">
+      <FiFacebook fill="blue"/>
+    </Link>
+  </div>
+
+    
+    </>
+  )
+}
---
+++ app/components/molecules/UnderMaintainance/UnderMaintainance.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+type Props = {}
+
+export default function UnderMaintainance({}: Props) {
+  return (
+    <div className="content">Under construction</div>
+  )
+}
