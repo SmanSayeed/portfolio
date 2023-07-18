5bd5897443c30f7d2d52970721286258d4086676
smansayeed <786saadman@gmail.com>
2023-07-18 12:05:43 +0600

    removed skills

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
       <Hero />
       <Skills/>
       <SkillsAndExperience />
-      <ProjectsAndServices/>
+      {/* <ProjectsAndServices/> */}
     </main>
   );
 }
